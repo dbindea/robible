a3d4586fccd04068ecbbe268953025129a55858b
Dorel Bindea <dbindea@gmail.com>
2022-03-21 09:38:49 +0100

    update script

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,22 @@
 <script>
-  /*   const getBible = async () => {
-    const res = await fetch("cornilescu.json");
-    const bible = res.json();
-    console.log(bible);
-  };
-  getBible(); */
+  const version = "vdc";
 
-  let getBible = fetch("vdc/bible.json").then((res) => res.json());
-  let getBibleMap = fetch("vdc/bible.map.json").then((res) => res.json());
+  const _getBible = async () => {
+    const bible = await fetch(`${version}/bible.json`).json();
+  };
+
+  const _getBibleMap = async () => {
+    const bibleMap = await fetch(`${version}/bible.map.json`).json();
+  };
+
+  _getBible();
+  _getBibleMap();
+
+  // let getBible = fetch("vdc/bible.json").then((res) => res.json());
+  // let getBibleMap = fetch("vdc/bible.map.json").then((res) => res.json());
 </script>
 
-{#await getBible}
+<!-- {#await getBible}
   <p>Loading Bible...</p>
 {:then result}
   {#await getBibleMap then map}
@@ -28,7 +34,26 @@
       </tbody>
     </table>
   {/await}
-{/await}
+{/await} -->
+
+<table>
+  <thead>
+    <tr>
+      <td>
+        Cartea
+      </td>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      {#each Object.keys(bible) as bookNumber}
+      <td>
+        Cartea
+      </td>
+      {/each}
+    </tr>
+  </tbody>
+</table>
 
 <style>
   p {
